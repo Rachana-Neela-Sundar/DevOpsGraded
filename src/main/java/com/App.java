69bc8ea987c4140027ead15cd4e2b61af461f712
rachana neela sundar <rachanarachu333@gmail.com>
2022-01-16 01:03:43 +0530

    update

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,24 +1,19 @@
 package com;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App implements CommandLineRunner {
+public class App  {
 
 	public static void main(String[] args) {
 		
 		SpringApplication.run(App.class, args);
-		System.err.println("Hello Dev-Ops");
+		System.out.println("Hello Dev-Ops");
 		
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Dev_Ops");
-		
-	}
+
 
 }
